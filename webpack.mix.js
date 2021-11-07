01777995c42c2b96d6098b9def4fe3d870899f02
Toghrul Nasirli <toghrulnasirli111@gmail.com>
2021-11-07 20:07:34 +0400

    admin and front files added through webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,7 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/admin.js', 'public/js')
+    .sass('resources/sass/admin.scss', 'public/css')
+    .js('resources/js/front.js', 'public/js')
+    .sass('resources/sass/front.scss', 'public/css')
     .sourceMaps();
