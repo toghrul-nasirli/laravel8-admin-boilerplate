1042b75a3e763ed10ee60bf3ce38d0c132f72eac
Toghrul Nasirli <toghrulnasirli111@gmail.com>
2021-11-07 20:59:01 +0400

    sweetalert2 added

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,7 +1,9 @@
 import './bootstrap';
+import Swal from 'sweetalert2';
 import Cleave from 'cleave.js';
 
 try {
+    window.Swal = Swal;
     window.Cleave = Cleave;
 } catch (error) {
     console.log(error);
