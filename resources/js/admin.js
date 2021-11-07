fa6a3046a5bca2f1de8a2ac98b83989dbb73139d
Toghrul Nasirli <toghrulnasirli111@gmail.com>
2021-11-07 23:23:00 +0400

    cleave.js removed

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,10 +1,8 @@
 import './bootstrap';
 import Swal from 'sweetalert2';
-import Cleave from 'cleave.js';
 
 try {
     window.Swal = Swal;
-    window.Cleave = Cleave;
 } catch (error) {
     console.log(error);
 }
