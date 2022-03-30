8a999353ccb1709c35f689f11c804655db1e830c
toghrul-nasirli <toghrulnasirli111@gmail.com>
2022-03-30 15:30:51 +0400

    tinymce configured

--- resources/views/partials/admin/_scripts.blade.php
+++ resources/views/partials/admin/_scripts.blade.php
@@ -1,4 +1,15 @@
 @livewireScripts
 <script src="{{ asset('js/admin.js') }}"></script>
 <script src="{{ asset('backend/js/adminlte.min.js') }}"></script>
+<script>
+    tinymce.init({
+        selector: '.editor',
+        plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak code lists',
+        toolbar: 'undo redo | styleselect | fontsizeselect | forecolor | bold italic | numlist bullist | alignleft aligncenter alignright alignjustify | outdent indent | link image | code', 
+        toolbar_mode: 'floating',
+        force_br_newlines : false,
+        force_p_newlines : false,
+        forced_root_block : '',
+    });
+</script>
 @yield('scripts')
