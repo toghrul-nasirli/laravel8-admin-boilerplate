224c1613deeefec379d1bbc2070715cbb81627f0
toghrul-nasirli <toghrulnasirli111@gmail.com>
2022-05-04 17:59:26 +0400

    minor refactoring

--- resources/views/livewire/admin/posts-table.blade.php
+++ resources/views/livewire/admin/posts-table.blade.php
@@ -78,35 +78,5 @@
 </div>
 
 @section('scripts')
-    <script>
-        window.addEventListener('Swal:confirm', event => {
-            Swal.fire({
-                icon: 'warning',
-                title: event.detail.title,
-                text: event.detail.text,
-                showCancelButton: true,
-                confirmButtonText: '@lang('admin.yes-delete-it')',
-                confirmButtonColor: '#3085d6',
-                cancelButtonText: '@lang('admin.cancel')',
-                cancelButtonColor: '#d33',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.livewire.emit('delete', event.detail.id);
-                    Swal.fire({
-                        toast: true,
-                        icon: 'success',
-                        title: '@lang('admin.deleted')',
-                        position: 'top-right',
-                        showConfirmButton: false,
-                        timerProgressBar: true,
-                        timer: 2000,
-                        didOpen: (toast) => {
-                            toast.addEventListener('mouseenter', Swal.stopTimer)
-                            toast.addEventListener('mouseleave', Swal.resumeTimer)
-                        }
-                    });
-                }
-            });
-        });
-    </script>
+    @include('partials.admin._swal')
 @endsection
